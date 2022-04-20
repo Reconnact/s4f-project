09a5d0de9418b8edc5b88427e252b6d04722270f
Hermann Witte <witteh@bzz.ch>
2022-04-20 16:08:27 +0200

    normal version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-//import './App.css';
-//import Login from './login';
+import './App.css';
+import Login from './login';
 
 
 function App() {
   return (
-    <div>Hello World</div>
+    <Login />
   );
 }
 
